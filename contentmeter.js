a398c7a2467a8a2f3b89a863302b7eb9605c0061
be-zet <bartosz.zadara@gmail.com>
2016-05-04 16:34:15 +0200

    up

--- contentmeter.js
+++ contentmeter.js
@@ -17,14 +17,14 @@ ContentMeter.prototype = {
 
 	init: function() {
 		var base     = this,
-			options  = base.options, 
+			options  = base.options,
 			settings = base.settings;
 
 		function getDOMElement(selector) {
 			var element;
 
-			if (typeof selector === "string" 
-				&& selector.indexOf("#") !== -1 
+			if (typeof selector === "string"
+				&& selector.indexOf("#") !== -1
 				&& selector.indexOf("#") === 0) {
 
 				element = document.getElementById(selector.slice(1));
@@ -49,8 +49,10 @@ ContentMeter.prototype = {
 		// If exclusive options set to true, leave only first one enabled
 		if (settings.barByWidth && settings.barByPosition) settings.barByPosition = false;
 
+		base.readContentDimensions();
+
 		base.createMeter();
-	},	
+	},
 
 	createMeter: function() {
 		var base = this,
@@ -60,11 +62,15 @@ ContentMeter.prototype = {
 
 		bar = document.createElement("div");
 		bar.classList.add("contentmeter-bar");
-		
+
 		if (base.settings.barByWidth) {
-			bar.style.width = base.getBarWidth() + "%";
+			if (base.content.visibleHeight > window.innerHeight) {
+				bar.style.width = base.getBarWidth() + "%";
+			} else {
+				bar.style.width = base.getBarWidth2() + "%";
+			}
 		}
-		
+
 		if (base.settings.barByPosition) {
 			bar.setAttribute("style", "position: absolute; width: 100%;");
 			bar.style.left = base.getBarPosition() + "%";
@@ -81,23 +87,29 @@ ContentMeter.prototype = {
 	},
 
 	bindUIEvents: function() {
-		var base = this, 
+		var base = this,
 			bar  = base.bar;
 
 		if (base.settings.barByWidth) {
-			window.addEventListener("scroll", function() {	
-				bar.style.width = base.getBarWidth() + "%";
-			});
+			if (base.content.visibleHeight > window.innerHeight) {
+				window.addEventListener("scroll", function() {
+					bar.style.width = base.getBarWidth() + "%";
+				});
+			} else {
+				base.contContainer.addEventListener("scroll", function() {
+					bar.style.width = base.getBarWidth2() + "%";
+				});
+			}
 		}
 
 		if (base.settings.barByPosition) {
-			window.addEventListener("scroll", function() {	
+			window.addEventListener("scroll", function() {
 				bar.style.left = base.getBarPosition() + "%";
 			});
 		}
 
 		if (base.settings.invisibilityClass) {
-			window.addEventListener("scroll", function() {	
+			window.addEventListener("scroll", function() {
 				base.updateClasses();
 			});
 		}
@@ -119,47 +131,75 @@ ContentMeter.prototype = {
 		var base = this;
 
 		base.content = {
-			height        : base.contContainer.scrollHeight, 
+			height        : base.contContainer.scrollHeight,
 			visibleHeight : base.contContainer.clientHeight,
 			offset        : base.contContainer.offsetTop
 		}
 
-		base.content.visibleHeight = 
-			base.content.visibleHeight > window.innerHeight ? 
-				window.innerHeight : base.content.visibleHeight;
+		// base.content.visibleHeight =
+		// 	base.content.visibleHeight > window.innerHeight ?
+		// 		window.innerHeight : base.content.visibleHeight;
+	},
+
+	checkLimit: function(limited, min, max) {
+		var number = limited;
+
+		number = number > min ? number : min;
+		number = number < max ? number : max;
+
+		return number;
 	},
 
 	getBarWidth: function() {
 		var base = this,
 			barW = 0;
 
-		if (!base.content) { 
-			base.readContentDimensions(); 
+		if (!base.content) {
+			base.readContentDimensions();
 		}
 
 		barW = (
-			base.getDocScrolltop() 
-			- base.content.offset 
+			base.getDocScrolltop()
+			- base.content.offset
 			+ base.content.visibleHeight
 			) / base.content.height * 100;
 
-		barW = barW > 0 ? barW : 0;
-		barW = barW < 100 ? barW : 100;
+		barW = base.checkLimit(barW, 0, 100);
+
+		console.log(base.content);
 
 		return barW;
-	}, 
+	},
+
+	getBarWidth2: function() {
+		var base = this,
+			barW = 0;
+
+		if (!base.content) {
+			base.readContentDimensions();
+		}
+
+		barW = (
+			base.contContainer.scrollTop
+			+ base.content.visibleHeight
+			) / base.content.height * 100;
+
+		barW = base.checkLimit(barW, 0, 100);
+
+		return barW;
+	},
 
 	getBarPosition: function() {
 		var base   = this,
 			barPos = 0;
 
-		if (!base.content) { 
-			base.readContentDimensions(); 
+		if (!base.content) {
+			base.readContentDimensions();
 		}
 
 		barPos = (
-			base.getDocScrolltop() 
-			- base.content.offset 
+			base.getDocScrolltop()
+			- base.content.offset
 			+ base.content.visibleHeight
 			) / base.content.height * 100;
 
@@ -169,12 +209,12 @@ ContentMeter.prototype = {
 		barPos = barPos - 100;
 
 		return barPos;
-	}, 
+	},
 
 	updateClasses: function() {
 		var base = this;
-		
-		if (   base.getDocScrolltop() > (base.content.height + base.content.offset) 
+
+		if (   base.getDocScrolltop() > (base.content.height + base.content.offset)
 			|| (base.getDocScrolltop() + base.content.visibleHeight) < base.content.offset) {
 
 			base.barContainer.classList.add("invisible");
