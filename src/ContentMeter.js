2aadc4e67924b3054fc4845f1eaf45f8b98b5039
bezet <bartosz.zadara@gmail.com>
2017-01-14 23:10:35 +0100

    Extract several methods to a separate class

--- src/ContentMeter.js
+++ src/ContentMeter.js
@@ -1,3 +1,5 @@
+import Utilities from './Utilities.js';
+
 class ContentMeter {
 	constructor( barSelector, contSelector, options ) {
 		this.barSelector = barSelector;
@@ -5,25 +7,6 @@ class ContentMeter {
 
 		this.init();
 	}
-
-  getDOMElement( selector ) {
-    let element = null;
-
-    if ( typeof selector === "string"
-      && selector.indexOf( "#" ) !== -1
-      && selector.indexOf( "#" ) === 0
-    ) {
-      element = document.getElementById( selector.slice(1) );
-
-    } else if ( typeof selector === "object"
-      && selector.nodeType !== undefined
-      && selector.length === undefined
-    ) {
-      element = selector;
-    }
-
-    return element;
-  }
 
   readContentDimensions() {
     this.content = {
@@ -78,31 +61,18 @@ class ContentMeter {
 		} );
 	}
 
-	getDocScrolltop() {
-		return document.documentElement.scrollTop || document.body.scrollTop;
-	}
-
-	limitTheNumber( theNumber, min, max ) {
-		let number = theNumber;
-
-		number = number > min ? number : min;
-		number = number < max ? number : max;
-
-		return number;
-	}
-
 	getBarWidth() {
 		let barW = 0;
 
 		if ( !this.content ) this.readContentDimensions();
 
 		barW = (
-			this.getDocScrolltop()
+			Utilities.getDocScrolltop()
 			- this.content.offset
 			+ this.content.visibleHeight
 		) / this.content.height * 100;
 
-		barW = this.limitTheNumber( barW, 0, 100 );
+		barW = Utilities.limitTheNumber( barW, 0, 100 );
 
 		return barW;
 	}
@@ -117,14 +87,14 @@ class ContentMeter {
 			+ this.content.visibleHeight
 		) / this.content.height * 100;
 
-		barW = this.limitTheNumber( barW, 0, 100 );
+		barW = Utilities.limitTheNumber( barW, 0, 100 );
 
 		return barW;
 	}
 
 	updateClasses() {
-		if ( this.getDocScrolltop() > ( this.content.height + this.content.offset )
-		  || ( this.getDocScrolltop() + this.content.visibleHeight ) < this.content.offset ) {
+		if ( Utilities.getDocScrolltop() > ( this.content.height + this.content.offset )
+		  || ( Utilities.getDocScrolltop() + this.content.visibleHeight ) < this.content.offset ) {
 
 			this.barContainer.classList.add( "js-invisible" );
 		} else {
@@ -133,8 +103,8 @@ class ContentMeter {
 	}
 
   init() {
-    this.barContainer = this.getDOMElement( this.barSelector );
-    this.contentContainer = this.getDOMElement( this.contSelector );
+    this.barContainer = Utilities.getDOMElement( this.barSelector );
+    this.contentContainer = Utilities.getDOMElement( this.contSelector );
 
     if ( this.barContainer !== null && this.contentContainer !== null ) {
       this.readContentDimensions();
---
+++ src/Utilities.js
@@ -0,0 +1,35 @@
+class Utilities {
+  static getDOMElement( selector ) {
+    let element = null;
+
+    if ( typeof selector === "string"
+      && selector.indexOf( "#" ) !== -1
+      && selector.indexOf( "#" ) === 0
+    ) {
+      element = document.getElementById( selector.slice(1) );
+
+    } else if ( typeof selector === "object"
+      && selector.nodeType !== undefined
+      && selector.length === undefined
+    ) {
+      element = selector;
+    }
+
+    return element;
+  }
+
+  static getDocScrolltop() {
+    return document.documentElement.scrollTop || document.body.scrollTop;
+  }
+
+  static limitTheNumber( theNumber, min, max ) {
+    let number = theNumber;
+
+    number = number > min ? number : min;
+    number = number < max ? number : max;
+
+    return number;
+  }
+}
+
+export default Utilities;
