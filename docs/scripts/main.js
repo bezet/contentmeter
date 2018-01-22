ca6e4dfaa9cff2079b05096036a32ce58bef910e
bezet <bartosz.zadara@gmail.com>
2018-01-22 18:26:04 +0100

    Import lib and init it

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,2 +1,4 @@
-// import Contentmeter from '@bezet/contentmeter';
-// const myContentmeter = new Contentmeter();
+import Contentmeter from '@bezet/contentmeter';
+const myContentmeter = new Contentmeter('#content_meter', '#content_container', {
+  invisibilityClass: false
+});
