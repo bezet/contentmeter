0e91f3f6df649f7f98c57d45bf7abf432b22378c
bezet <bartosz.zadara@gmail.com>
2018-01-25 22:47:20 +0100

    Up dist & docs

--- docs/bundle.js
+++ docs/bundle.js
@@ -76,8 +76,9 @@ var _contentmeter2 = _interopRequireDefault(_contentmeter);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var myContentmeter = new _contentmeter2.default('#content_meter', '#content_container', {
-  invisibilityClass: false
+var myContentmeter = new _contentmeter2.default({
+  barSelector: '#content_meter',
+  contentSelector: '#content_container'
 });
 
 /***/ }),
@@ -102,48 +103,34 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Contentmeter = function () {
-  function Contentmeter(barSelector, contentSelector) {
+  function Contentmeter() {
+    var _this = this;
+
+    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
     _classCallCheck(this, Contentmeter);
 
-    this.barSelector = barSelector;
-    this.contentSelector = contentSelector;
+    this.settings = {
+      barSelector: '#content_meter',
+      contentSelector: '#content_container'
+    };
 
-    this.init();
+    Object.keys(options).forEach(function (option) {
+      _this.settings[option] = options[option];
+    });
+
+    this.barContainer = document.querySelector(this.settings.barSelector);
+    this.contentContainer = document.querySelector(this.settings.contentSelector);
+
+    if (this.barContainer !== null && this.contentContainer !== null) {
+      this.readContentDimensions();
+      this.createMeter();
+    } else {
+      throw new Error('Wrong selectors or given selectors match no elements.');
+    }
   }
 
   _createClass(Contentmeter, [{
-    key: 'updateClasses',
-    value: function updateClasses() {
-      if (_utilities2.default.getDocScrolltop() > this.content.height + this.content.offset || _utilities2.default.getDocScrolltop() + this.content.visibleHeight < this.content.offset) {
-
-        this.barContainer.classList.add('js-invisible');
-      } else {
-        this.barContainer.classList.remove('js-invisible');
-      }
-    }
-  }, {
-    key: 'getBarWidth',
-    value: function getBarWidth() {
-      var barW = 0;
-      if (!this.content) {
-        this.readContentDimensions();
-      }
-
-      barW = ((this.content.selfScrolled ? this.contentContainer.scrollTop : _utilities2.default.getDocScrolltop()) + this.content.barBasicVal) / this.content.height * 100;
-
-      return _utilities2.default.limitTheNumber(barW, 0, 100);
-    }
-  }, {
-    key: 'setBarWidth',
-    value: function setBarWidth() {
-      this.bar.style.width = this.getBarWidth() + '%';
-    }
-  }, {
-    key: 'setCounterValue',
-    value: function setCounterValue() {
-      this.counter.textContent = Math.ceil(this.getBarWidth());
-    }
-  }, {
     key: 'readContentDimensions',
     value: function readContentDimensions() {
       this.content = this.content || {};
@@ -153,61 +140,73 @@ var Contentmeter = function () {
       this.content.height = this.contentContainer.scrollHeight;
       this.content.visibleHeight = Math.min(window.innerHeight, this.contentContainer.clientHeight);
       this.content.offset = this.content.selfScrolled ? 0 : this.contentContainer.offsetTop;
-      this.content.barBasicVal = this.content.offset * -1 + this.content.visibleHeight;
-
-      console.log(this.content);
+      this.content.barBasicVal = -1 * this.content.offset + this.content.visibleHeight;
     }
   }, {
-    key: 'bindUIEvents',
-    value: function bindUIEvents() {
-      var _this = this;
+    key: 'calcBarScale',
+    value: function calcBarScale() {
+      if (!this.content) {
+        this.readContentDimensions();
+      }
+
+      var containerScrollTop = this.content.selfScrolled ? this.contentContainer.scrollTop : _utilities2.default.getDocScrolltop();
+      var barW = (containerScrollTop + this.content.barBasicVal) / this.content.height;
+
+      return _utilities2.default.limitTheNumber(barW, 0, 1);
+    }
+  }, {
+    key: 'setBarWidth',
+    value: function setBarWidth() {
+      this.bar.style.transform = 'scale(' + this.calcBarScale() + ', 1)';
+    }
+  }, {
+    key: 'setCounterValue',
+    value: function setCounterValue() {
+      this.counter.textContent = '' + Math.ceil(this.calcBarScale() * 100);
+    }
+  }, {
+    key: 'resizeHandler',
+    value: function resizeHandler() {
+      this.readContentDimensions();
+      this.setBarWidth();
+      this.setCounterValue();
+    }
+  }, {
+    key: 'scrollHandler',
+    value: function scrollHandler() {
+      this.setBarWidth();
+      this.setCounterValue();
+    }
+  }, {
+    key: 'bindEvents',
+    value: function bindEvents() {
+      var _this2 = this;
 
       var scrollTarget = this.content.selfScrolled ? this.contentContainer : window;
-      scrollTarget.addEventListener('scroll', function () {
-        _this.setBarWidth();
-        _this.setCounterValue();
-      });
 
-      window.addEventListener('scroll', function () {
-        return _this.updateClasses();
+      scrollTarget.addEventListener('scroll', function () {
+        return _this2.scrollHandler();
       });
       window.addEventListener('resize', function () {
-        _this.readContentDimensions();
-        _this.setBarWidth();
-        _this.setCounterValue();
-        _this.updateClasses();
+        return _this2.resizeHandler();
       });
     }
   }, {
     key: 'createMeter',
     value: function createMeter() {
-      var _this2 = this;
+      var _this3 = this;
 
       _utilities2.default.createDOMElement('div', 'contentmeter__bar', this.barContainer, function (element) {
-        _this2.bar = element;
-        _this2.setBarWidth();
+        _this3.bar = element;
+        _this3.setBarWidth();
       });
 
       _utilities2.default.createDOMElement('div', 'contentmeter__counter', this.barContainer, function (element) {
-        _this2.counter = element;
-        _this2.setCounterValue();
+        _this3.counter = element;
+        _this3.setCounterValue();
       });
 
-      this.updateClasses();
-      this.bindUIEvents();
-    }
-  }, {
-    key: 'init',
-    value: function init() {
-      this.barContainer = _utilities2.default.getDOMElement(this.barSelector);
-      this.contentContainer = _utilities2.default.getDOMElement(this.contentSelector);
-
-      if (this.barContainer !== null && this.contentContainer !== null) {
-        this.readContentDimensions();
-        this.createMeter();
-      } else {
-        throw new Error('Wrong selectors or given selectors match no elements.');
-      }
+      this.bindEvents();
     }
   }]);
 
@@ -228,8 +227,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -240,20 +237,7 @@ var Utilities = function () {
   }
 
   _createClass(Utilities, null, [{
-    key: 'getDOMElement',
-    value: function getDOMElement(selector) {
-      var element = null;
-
-      if (typeof selector === 'string' && selector.indexOf('#') !== -1 && selector.indexOf('#') === 0) {
-        element = document.getElementById(selector.slice(1));
-      } else if ((typeof selector === 'undefined' ? 'undefined' : _typeof(selector)) === 'object' && selector.nodeType !== undefined && selector.length === undefined) {
-        element = selector;
-      }
-
-      return element;
-    }
-  }, {
-    key: 'createDOMElement',
+    key: "createDOMElement",
     value: function createDOMElement(element, className, parent, callback) {
       var DOMElement = document.createElement(element);
       DOMElement.classList.add(className);
@@ -269,12 +253,12 @@ var Utilities = function () {
       return DOMElement;
     }
   }, {
-    key: 'getDocScrolltop',
+    key: "getDocScrolltop",
     value: function getDocScrolltop() {
       return document.documentElement.scrollTop || document.body.scrollTop;
     }
   }, {
-    key: 'limitTheNumber',
+    key: "limitTheNumber",
     value: function limitTheNumber(theNumber, min, max) {
       return theNumber > min ? theNumber < max ? theNumber : max : min;
     }
@@ -284,7 +268,7 @@ var Utilities = function () {
 }();
 
 exports.default = Utilities;
-module.exports = exports['default'];
+module.exports = exports["default"];
 
 /***/ })
 /******/ ]);
