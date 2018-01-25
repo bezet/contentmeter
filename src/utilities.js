1a45cfca835bafd737b31218f930198d2803c35e
bezet <bartosz.zadara@gmail.com>
2018-01-25 22:23:46 +0100

    Meet eslint requirements & remove useless method

--- src/utilities.js
+++ src/utilities.js
@@ -1,33 +1,14 @@
 class Utilities {
-  static getDOMElement( selector ) {
-    let element = null;
+  static createDOMElement(element, className, parent, callback) {
+    const DOMElement = document.createElement(element);
+    DOMElement.classList.add(className);
 
-    if ( typeof selector === 'string'
-      && selector.indexOf( '#' ) !== -1
-      && selector.indexOf( '#' ) === 0
-    ) {
-      element = document.getElementById( selector.slice( 1 ) );
-
-    } else if ( typeof selector === 'object'
-      && selector.nodeType !== undefined
-      && selector.length === undefined
-    ) {
-      element = selector;
+    if (parent) {
+      parent.appendChild(DOMElement);
     }
 
-    return element;
-  }
-
-  static createDOMElement( element, className, parent, callback ) {
-    const DOMElement = document.createElement( element );
-    DOMElement.classList.add( className );
-
-    if ( parent ) {
-      parent.appendChild( DOMElement );
-    }
-
-    if ( callback ) {
-      callback( DOMElement );
+    if (callback) {
+      callback(DOMElement);
     }
 
     return DOMElement;
@@ -37,8 +18,8 @@ class Utilities {
     return document.documentElement.scrollTop || document.body.scrollTop;
   }
 
-  static limitTheNumber( theNumber, min, max ) {
-    return ( theNumber > min ) ? ( ( theNumber < max ) ? theNumber : max ) : min;
+  static limitTheNumber(theNumber, min, max) {
+    return (theNumber > min) ? ((theNumber < max) ? theNumber : max) : min;
   }
 }
 
